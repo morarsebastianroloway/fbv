c12edec6344d01d17b5adfa4f9daab8621a35d12
morarsebastian <morarsebastian@gmail.com>
2023-06-18 12:49:28 +0300

    FBV-1 - Removed weather data, removed unnecessary usings

--- FBV.DAL/Contracts/IUnitOfWork.cs
+++ FBV.DAL/Contracts/IUnitOfWork.cs
@@ -1,12 +1,4 @@
-﻿using FBV.DAL.Repositories;
-using FBV.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FBV.DAL.Contracts
+﻿namespace FBV.DAL.Contracts
 {
     public interface IUnitOfWork
     {
--- FBV.DAL/Repositories/UnitOfWork.cs
+++ FBV.DAL/Repositories/UnitOfWork.cs
@@ -1,11 +1,5 @@
 ﻿using FBV.DAL.Contracts;
 using FBV.DAL.Data;
-using FBV.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FBV.DAL.Repositories
 {
